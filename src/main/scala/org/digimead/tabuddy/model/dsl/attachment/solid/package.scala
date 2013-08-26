0a3aaa7b28560791dfcdd4e73a1ac1d2ffedccda
Alexey Aksenov <ezh@ezh.msk.ru>
2013-08-26 23:30:48 +0400

    Fix header of source files.

--- src/main/scala/org/digimead/tabuddy/model/dsl/attachment/solid/package.scala
+++ src/main/scala/org/digimead/tabuddy/model/dsl/attachment/solid/package.scala
@@ -1,44 +1,19 @@
 /**
- * This file is part of the TABuddy project.
+ * SolidAttachment type for TABuddy-Model - a human-centric K,V framework
+ *
  * Copyright (c) 2013 Alexey Aksenov ezh@ezh.msk.ru
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Global License version 3
- * as published by the Free Software Foundation with the addition of the
- * following permission added to Section 15 as permitted in Section 7(a):
- * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED
- * BY Limited Liability Company «MEZHGALAKTICHESKIJ TORGOVYJ ALIANS»,
- * Limited Liability Company «MEZHGALAKTICHESKIJ TORGOVYJ ALIANS» DISCLAIMS
- * THE WARRANTY OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Global License for more details.
- * You should have received a copy of the GNU Affero General Global License
- * along with this program; if not, see http://www.gnu.org/licenses or write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA, 02110-1301 USA, or download the license from the following URL:
- * http://www.gnu.org/licenses/agpl.html
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Global License.
- *
- * In accordance with Section 7(b) of the GNU Affero General Global License,
- * you must retain the producer line in every report, form or document
- * that is created or manipulated using TABuddy.
- *
- * You can be released from the requirements of the license by purchasing
- * a commercial license. Buying such a license is mandatory as soon as you
- * develop commercial activities involving the TABuddy software without
- * disclosing the source code of your own applications.
- * These activities include: offering paid services to customers,
- * serving files in a web or/and network application,
- * shipping TABuddy with a closed source product.
- *
- * For more information, please contact Digimead Team at this
- * address: ezh@ezh.msk.ru
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.digimead.tabuddy.model.dsl.attachment
